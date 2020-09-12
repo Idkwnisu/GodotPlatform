32aec89cc7806c8836737bd042ab53ec4ea1149b
Stefano Palma <stefano.palma1@studenti.unimi.it>
2020-09-12 22:09:33 +0200

    First commit

---
+++ Actors/KinematicBody2D.gd
@@ -0,0 +1,185 @@
+extends KinematicBody2D
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+const RUN:int = 0;
+const ATTACK:int = 1;
+
+
+var target: Node2D;
+
+var lastTargetLeft: bool = false;
+
+var leftTarget: Node2D;
+
+var rightTarget: Node2D;
+
+var needNewTarget: bool = true;
+
+var targetingPlayer: bool = false;
+
+export var attackDistance = 100;
+
+var playerNode: Node2D;
+
+export var aggroDistance: float = 30.0;
+
+export var gravity: float;
+
+export var maxSpeed = Vector2(80,100);
+
+var playback : AnimationNodeStateMachinePlayback;
+
+var sprite;
+
+var _velocity: Vector2 = Vector2.ZERO;
+
+export var horizontalSpeed: float =  5;
+
+export var jumpSpeed = -600;
+
+export var health = 3;
+
+const FLOOR_NORMAL: Vector2 = Vector2.UP
+
+var jumping = false;
+
+var groundTime = 0.1;
+
+var lastGround = 0;
+
+var current_animation:int = RUN;
+
+var hitted : bool = false;
+
+var lastHit = 0;
+
+var hitTime = 0.3;
+
+var AttackCollision: CollisionShape2D;
+
+var LeftAttack: Node2D;
+
+var RightAttack: Node2D;
+
+var dead : bool = false;
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	playback = get_node("Tree").get("parameters/playback");
+	sprite = get_node("Pivot");
+	leftTarget = get_node("../LeftBound");
+	rightTarget = get_node("../RightBound");
+	playerNode = get_node("../../Player/KingBody/Pivot");
+	AttackCollision = get_node("AttackArea/AttackCollision");
+	LeftAttack = get_node("LeftAttack");
+	RightAttack = get_node("RightAttack");
+
+	
+func _physics_process(delta):
+
+	if not hitted:
+		update_target();
+		var hor_direction = get_horizontal_direction();
+		var hor = hor_direction*horizontalSpeed;
+		if not is_on_floor():
+			hor = 0;
+		var movement: Vector2 = Vector2(hor,gravity);
+		_velocity += movement;
+	
+		if get_global_position().distance_to(playerNode.get_global_position()) < attackDistance:
+			_velocity.x = 0;
+	
+			
+		if _velocity.x > maxSpeed.x*( 2.0 if targetingPlayer else 1.0) :
+			_velocity.x = maxSpeed.x*( 2.0 if targetingPlayer else 1.0) ;
+		if _velocity.x < -maxSpeed.x*( 2.0 if targetingPlayer else 1.0) :
+			_velocity.x = -maxSpeed.x*( 2.0 if targetingPlayer else 1.0) ;
+				
+		if _velocity.y > maxSpeed.y:
+			_velocity.y = maxSpeed.y;
+		if _velocity.y < -maxSpeed.y:
+			_velocity.y = -maxSpeed.y;
+		
+		if position.distance_to(target.position) < 30 and not targetingPlayer:
+			needNewTarget = true;
+		if not dead:
+			if hor_direction > 0:
+				sprite.scale = Vector2(-3,3);
+			else:
+				sprite.scale = Vector2(3,3);
+			
+	if dead:
+		_velocity.x = 0;	
+	_velocity = move_and_slide(_velocity, FLOOR_NORMAL);
+	
+func _process(delta):
+	
+	if not dead:
+		if hitted:
+			lastHit += delta;
+			if lastHit > hitTime:
+				hitted = false;
+				playback.travel("Run")
+				current_animation = RUN;
+		if not hitted:
+			if get_global_position().distance_to(playerNode.get_global_position()) < attackDistance:
+				if current_animation == RUN:
+					playback.travel("Attack");
+					current_animation = ATTACK;
+					AttackCollision.set_disabled(false);
+					if playerNode.get_global_position().x > get_global_position().x:
+						AttackCollision.position = RightAttack.position;
+					else:
+						AttackCollision.position = LeftAttack.position;
+			else:
+				if current_animation == ATTACK:
+					current_animation = RUN;
+					playback.travel("Run");
+					AttackCollision.set_disabled(true);
+	
+func get_horizontal_direction() -> float:
+	if get_global_position().x > target.get_global_position().x:
+		return -1.0;
+	else:
+		return 1.0;
+	
+func update_target():
+	if get_global_position().distance_to(playerNode.get_global_position()) < aggroDistance:
+		target = playerNode;
+		targetingPlayer = true;
+	elif needNewTarget or targetingPlayer:
+		if lastTargetLeft:
+			target = rightTarget;
+			lastTargetLeft = false;
+			needNewTarget = false;
+			targetingPlayer = false;
+		else:
+			target = leftTarget;
+			lastTargetLeft = true;
+			needNewTarget = false; 	
+			targetingPlayer = false;
+		
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+
+func _on_AttackArea_body_entered(body):
+	if not dead:
+		
+		health = health -1;
+		if(health < 0):
+			playback.travel("Death");
+			dead = true;
+			
+		else:
+			playback.travel("Hit");
+			hitted = true;
+			lastHit = 0.0;
+		if playerNode.get_global_position().x > get_global_position().x:
+			_velocity += Vector2(-200,-50);
+		else:
+			_velocity += Vector2(200,-50);
---
+++ Actors/King.gd
@@ -0,0 +1,129 @@
+extends KinematicBody2D
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+export var gravity: float;
+
+export var maxSpeed = Vector2(80,300);
+
+var playback : AnimationNodeStateMachinePlayback;
+
+var sprite;
+
+var _velocity: Vector2 = Vector2.ZERO;
+
+export var horizontalSpeed: float =  5;
+
+export var jumpSpeed = -600;
+
+const FLOOR_NORMAL: Vector2 = Vector2.UP
+
+var jumping = false;
+
+var groundTime = 0.1;
+
+var attackTime = 0.3;
+
+var startAttack = 0;
+
+var attacking = false;
+
+var lastGround = 0;
+
+var AttackLeft: Node2D;
+var AttackRight: Node2D;
+
+var AttackCollision: CollisionShape2D;
+
+var goingRight : bool = true;
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	playback =  get_node("Tree").get("parameters/playback");
+	sprite = get_node("Pivot");
+	AttackLeft = get_node("../AttackLeft");
+	AttackRight = get_node("../AttackRight");
+	AttackCollision = get_node("AttackArea/AttackCollision");
+
+func _process(delta):
+	if attacking:
+		startAttack += delta;
+		if startAttack > attackTime:
+			AttackCollision.set_disabled(true);
+			attacking = false;
+			playback.travel("KingIdle");
+	lastGround += delta;
+	if lastGround > groundTime and not attacking:
+		if Input.is_action_pressed("Left_Move") or Input.is_action_pressed("Right_Move") and not jumping:
+			playback.travel("KingRun");
+		elif not jumping:
+			playback.travel("KingIdle");
+		
+	if Input.is_action_pressed("Left_Move") and not Input.is_action_pressed("Right_Move"):
+		goingRight = false;
+		sprite.scale = Vector2(-3,3);
+	elif not Input.is_action_pressed("Left_Move") and Input.is_action_pressed("Right_Move"):
+		goingRight = true;
+		sprite.scale = Vector2(3,3);
+		
+	if Input.is_action_just_pressed("Attack") and not attacking:
+		playback.travel("KingAttack");
+		attacking = true;
+		AttackCollision.set_disabled(false);
+		if goingRight:
+			AttackCollision.position = AttackRight.position;
+		else:
+			AttackCollision.position = AttackLeft.position;
+		startAttack = 0;
+	
+func _physics_process(delta):
+	
+	
+	var horizontal = Input.get_action_strength("Right_Move") - Input.get_action_strength("Left_Move")
+	var movement;
+	if _velocity.y < 0:
+		movement = Vector2(horizontal*horizontalSpeed,2*gravity);
+	else:
+		movement = Vector2(horizontal*horizontalSpeed,2*gravity);
+		
+		
+			
+	if jumping and is_on_floor():
+		jumping = false;
+		playback.travel("KingGround");
+		lastGround = 0;
+		
+		
+	if Input.is_action_just_pressed("Jump") and is_on_floor():
+		_velocity.y = jumpSpeed;
+		jumping = true;
+		playback.travel("KingJump");
+
+	if Input.is_action_just_released("Jump") and _velocity.y < 0:
+		_velocity.y = 0;
+	
+	
+	_velocity += movement;
+	
+		
+	if _velocity.x > maxSpeed.x:
+		_velocity.x = maxSpeed.x;
+	if _velocity.x < -maxSpeed.x:
+		_velocity.x = -maxSpeed.x;
+		
+	if _velocity.y > maxSpeed.y:
+		_velocity.y = maxSpeed.y;
+	if _velocity.y < -maxSpeed.y:
+		_velocity.y = -maxSpeed.y;
+		
+	if horizontal == 0 or (horizontal < 0 and _velocity.x > 0) or (horizontal > 0 and _velocity.x < 0) :
+		_velocity *= Vector2(0.7,1);
+	
+	_velocity = move_and_slide(_velocity, FLOOR_NORMAL);
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
